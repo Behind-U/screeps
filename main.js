403221bcc9beda411494ce3f0d4fb932bcdd85f6
Behind-U <iriko@p1.s-cat.ne.jp>
2016-11-26 20:41:27 +0900

    git test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
-    
+
         for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
